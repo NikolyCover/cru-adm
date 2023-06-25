bc13f7e1ba7d3793631f08535f02a1e072acbdc2
Nikoly Cover <nikolycoverpereira@gmail.com>
2023-06-25 15:01:22 -0300

    feature: created navigation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,14 +2,21 @@ import { Stack } from '@mui/system'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 import { Button } from '@mui/material'
 import { theme } from '../../theme'
+import { useNavigate } from 'react-router'
 
 export const Header = () => {
+    const navigate = useNavigate()
+
+    const handleClick = (path: string) => {
+        navigate(`/${path}`)
+    }
+
 	return (
 		<Stack direction="row" justifyContent="space-between" sx={{ marginY: theme.spacing(2) }}>
 			<Logo />
 			<Stack direction="row" gap={4}>
-				<Button variant="text">Cardápios</Button>
-				<Button variant="text">Menus</Button>
+				<Button variant="text" onClick={() => handleClick('menus')}>Cardápios</Button>
+				<Button variant="text" onClick={() => handleClick('dishes')}>Pratos</Button>
 			</Stack>
 		</Stack>
 	)
