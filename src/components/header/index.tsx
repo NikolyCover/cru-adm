de21fe3280769253f6df25d8b2e5119e4c0431b9
Nikoly Cover <nikolycoverpereira@gmail.com>
2023-06-30 19:56:36 -0300

    feature: changed logotype

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import { Stack } from '@mui/system'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { ReactComponent as Logo } from '../../assets/logotype.svg'
 import { Button } from '@mui/material'
 import { theme } from '../../theme'
 import { useNavigate } from 'react-router'
@@ -12,7 +12,7 @@ export const Header = () => {
     }
 
 	return (
-		<Stack direction="row" justifyContent="space-between" sx={{ paddingY: theme.spacing(2), mb: theme.spacing(4) }}>
+		<Stack direction="row" justifyContent="space-between" sx={{ paddingY: theme.spacing(4), mb: theme.spacing(2) }}>
 			<Logo />
 			<Stack direction="row" gap={4}>
 				<Button variant="text" size='small' onClick={() => handleClick('menus')}>Cardápios</Button>
