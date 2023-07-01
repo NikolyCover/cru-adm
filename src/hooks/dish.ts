f2ccdfca9e3ba7e85f07a7f0b2fcbd13b1be2967
Nikoly Cover <nikolycoverpereira@gmail.com>
2023-06-30 21:56:33 -0300

    feature: treated delete and created dish exceptions

--- src/hooks/dish.ts
+++ src/hooks/dish.ts
@@ -13,6 +13,7 @@ import { AxiosError } from 'axios'
 import { createDish, deleteDish, updateDish } from '../services/dish'
 import { DishParamns } from '../schemas/dish'
 import { dishesAtom } from '../contexts/dish'
+import { HTTPStatus } from '../interfaces/http-status'
 
 export const useDish = () => {
 	const setDishes = useSetRecoilState(dishesAtom)
@@ -20,25 +21,18 @@ export const useDish = () => {
 
 	const del = async (id: number) => {
 		try {
-			const { data } = await deleteDish(id)
+			await deleteDish(id)
 
-			if(!data) {
-				setFeedback({
-					value: 'error',
-					message: DELETE_DISH_REGISTERED_IN_MENU_MESSAGE,
-				})
-			} else {
-				setDishes((dishes) => [...dishes.filter((dish) => dish.id !== id)])
+			setDishes((dishes) => [...dishes.filter((dish) => dish.id !== id)])
 
-				setFeedback({
-					value: 'success',
-					message: DELETE_DISH_SUCESS_MESSAGE,
-				})
-			}
+			setFeedback({
+				value: 'success',
+				message: DELETE_DISH_SUCESS_MESSAGE,
+			})
 		} catch (error) {
 			setFeedback({
 				value: 'error',
-				message: DELETE_DISH_ERROR_MESSAGE + (error as AxiosError).message,
+				message: (error as AxiosError<HTTPStatus>).response?.data.message ?? DELETE_DISH_ERROR_MESSAGE,
 			})
 		}
 	}
@@ -56,7 +50,7 @@ export const useDish = () => {
 		} catch (error) {
 			setFeedback({
 				value: 'error',
-				message: CREATE_DISH_ERROR_MESSAGE,
+				message: (error as AxiosError<HTTPStatus>).response?.data.message ?? CREATE_DISH_ERROR_MESSAGE,
 			})
 		}
 	}
@@ -65,7 +59,7 @@ export const useDish = () => {
 		try {
 			const { data } = await updateDish(params, id)
 
-			setDishes((dishes) => [...(dishes.filter((dish) => dish.id !== id)), data].sort((a, b) => a.id - b.id))
+			setDishes((dishes) => [...dishes.filter((dish) => dish.id !== id), data].sort((a, b) => a.id - b.id))
 
 			setFeedback({
 				value: 'success',
---
+++ src/interfaces/http-status.ts
@@ -0,0 +1,4 @@
+export interface HTTPStatus {
+    message: string
+    statusCode: number    
+}
